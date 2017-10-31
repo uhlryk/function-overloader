f68056b218c655165f677accf5a907b49205df8d
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2017-10-31 22:59:02 +0100

    checkTypeCondition test

---
+++ src/checkTypeCondition.spec.js
@@ -0,0 +1,17 @@
+import Overload from "./Overload";
+import checkTypeCondition from "./checkTypeCondition";
+
+describe("checkTypeCondition", () => {
+    describe("condition argument met test argument", () => {
+        it("should be true", () => {
+            expect(checkTypeCondition(Overload.NUMBER, 10)).to.be.true();
+            expect(checkTypeCondition(Overload.NUMBER(), 10)).to.be.true();
+        });
+    });
+    describe("condition argument not met test argument", () => {
+        it("should be false", () => {
+            expect(checkTypeCondition(Overload.NUMBER, true)).to.be.false();
+            expect(checkTypeCondition(Overload.NUMBER(), true)).to.be.false();
+        });
+    });
+});
